0b0522a44f67a479bb2ec1d563c9a9fa0ca71245
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-07-26 11:01:06 +0300

    add IP address to CORS origins

--- init/app.ts
+++ init/app.ts
@@ -12,7 +12,7 @@ async function start(app: Express): Promise<void> {
     const port: string | undefined = process.env.PORT || '5500';
     app.use(express.json());
     app.use(cors({
-        origin: 'http://localhost:3000',
+        origin: ['http://localhost:3000', 'http://192.168.0.104:3000'],
         credentials: true,
     }));
 

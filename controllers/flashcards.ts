d32e0af65e8c734818ad6d7edbd2f175e231943e
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-05 09:49:54 +0300

    add get routes to flashcard controller

--- controllers/flashcards.ts
+++ controllers/flashcards.ts
@@ -1,34 +1,36 @@
 import Express, { Response } from 'express'
+// import { isAuthor } from '../middlewares/guards.js';
 import flashcardService from '../services/flashcards.js';
 import jwtService from '../services/jwt.js';
 import mapErrors from '../util/errorMapper.js';
 import { router } from './auth.js';
 
-router.get('/saved', jwtService.verifyToken, async (_req: Express.Request, res: Response) => {
-    res.status(200).json([
-        {
-            title: 'War',
-            author: 'Ryota',
-            authorId: '15',
-            date: '02-02-2022',
-            id: 1,
-        },
-        {
-            title: 'Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem Lorem lorem ',
-            author: 'admin',
-            authorId: '112',
-            date: '04-06-2050',
-            id: 2,
-        }
-    ]);
+router.get('/flashcard/saved', jwtService.verifyToken, async (req: Express.Request, res: Response) => {
+    try {
+        const id: string = req.accessToken._id;        
+        const decks: IDeck[] = await flashcardService.getDecks(id)
+        
+        res.status(200).json(decks).end();
+    } catch (err) {
+        const errors = mapErrors(err);
+        res.status(404).json(errors).end();
+    }
+});
 
-    res.end();
+router.get('/flashcard/:id', jwtService.verifyToken, async (req: Express.Request, res: Response) => {
+    const id: string = req.params.id;
+    const deck = await flashcardService.getDeck(id);
+    const flashcards: IFlashcard[] = await flashcardService.getFlashcards(deck);
+    res.status(200).json({
+        deck,
+        flashcards,
+    }).end();
 });
 
 router.post('/flashcard/create', jwtService.verifyToken, async (req: Express.Request, res: Response) => {
     let deck: IDeck | null = null;
     let flashcards: IFlashcard[] = [];
-    
+
     try {
         flashcards = await Promise.all(req.body.flashcards.map(async (f: any) => await flashcardService.createFlashcard(f)));
     } catch (err) {
@@ -41,7 +43,6 @@ router.post('/flashcard/create', jwtService.verifyToken, async (req: Express.Req
         res.status(201).json(deck._id).end();
     } catch (err) {
         const errors: string[] = mapErrors(err);
-        console.log(errors);
         res.status(403).json(errors).end();
     }
 });

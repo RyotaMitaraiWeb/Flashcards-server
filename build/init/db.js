a884e40c593eadcd6bb6a1e1d7c2b465c0ceb3b5
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-13 13:00:14 +0300

    mongodb

--- build/init/db.js
+++ build/init/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 function connectToDB() {
     const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/flashcards';
     mongoose.set('runValidators', true);
-    mongoose.set('bufferCommands', false);
-    mongoose.set('bufferMaxEntries', 0);
     mongoose.connect(url).then(() => {
         console.log('DB connected');
     });
--- init/db.ts
+++ init/db.ts
@@ -4,8 +4,6 @@ function connectToDB(): void {
     const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/flashcards';
 
     mongoose.set('runValidators', true);
-    mongoose.set('bufferCommands', false);
-    mongoose.set('bufferMaxEntries', 0);
     mongoose.connect(url).then(() => {
         console.log('DB connected');
         

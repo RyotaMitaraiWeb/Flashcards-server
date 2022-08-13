44088d7e8255f9b8023c8c5232c09a48cefaddaa
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-13 12:05:18 +0300

    mongodb

--- build/init/db.js
+++ build/init/db.js
@@ -4,7 +4,8 @@ const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/flashcards';
 mongoose.set('runValidators', true);
 function connectToDB() {
     return __awaiter(this, void 0, void 0, function* () {
-        mongoose.connect(url);
+        mongoose.set("useNewUrlParser", true);
+        yield mongoose.connect(url);
         console.log('connected to DB');
     });
 }
--- init/db.ts
+++ init/db.ts
@@ -5,7 +5,8 @@ const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/flashcards';
 mongoose.set('runValidators', true);
 
 async function connectToDB(): Promise<void> {
-    mongoose.connect(url);
+    mongoose.set("useNewUrlParser", true);
+    await mongoose.connect(url);
 
     console.log('connected to DB');
 }

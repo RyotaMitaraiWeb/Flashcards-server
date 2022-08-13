8b19dad03abb3a0fae118f3f2ba9f411099ffa66
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-08-13 13:19:13 +0300

    tests deploy

--- init/app.ts
+++ init/app.ts
@@ -11,7 +11,7 @@ async function start(app: Express): Promise<void> {
     const port = process.env.PORT || 5500;
     app.use(express.json());
     app.use(cors({
-        origin: ['http://localhost:3000', 'http://192.168.0.104:3000'],
+        origin: ['http://localhost:3000', 'http://192.168.0.104:3000', 'https://flashcards-da911.web.app/'],
         credentials: true,
     }));
 
